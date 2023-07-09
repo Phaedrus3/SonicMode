1f5461a4983f4013cc1a265f1c4fb67de10a318f
Phaedrus3 <131749747+Phaedrus3@users.noreply.github.com>
2023-07-09 17:03:16 -0500

    Add files via upload

---
+++ BuildInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace SonicMode;
+
+public static class BuildInfo
+{
+	public const string Name = "SonicMode";
+	public const string Description = "Lets you go fast!";
+	public const string Author = "ds5678, phaedrus";
+	public const string Version = "2.1.0";
+}
